eb0b06556dc302c2345269090015e3521e4a79a0
evargas21 <evargas7324@gmail.com>
2021-04-11 20:31:10 -0500

    new changes

--- prueba tecnica/db/register.js
+++ prueba tecnica/db/register.js
@@ -2,7 +2,7 @@ var firebaseConfig = {
     apiKey: "AIzaSyDKm2AGsuink1OzcV43XhmXqt5rhyCjiCc",
     authDomain: "prueba-tecnica-2cff7.firebaseapp.com",
     projectId: "prueba-tecnica-2cff7",
-    storageBucket: "prueba-tecnica-2cff7.appspot.com",
+    storageBucket: "gs://prueba-tecnica-2cff7.appspot.com",
     messagingSenderId: "74003447411",
     appId: "1:74003447411:web:39249d5aba9d29a9153406"
 };
@@ -10,17 +10,29 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 //initialize firestore
 const db = firebase.firestore()
+// let storage = firebase.storage();
 
+let myFile = ""
+
+const adjunto = document.getElementById('customFile')   
+
+adjunto.addEventListener('change', async (e) => {
+    //Obtener archivo
+    let file = e.target.files[0];
+    myFile = file
+    console.log(myFile.name)
+})
 
 // Save data 
 function saveData() {
     const motivo = document.getElementById('motivo').value
     const fechaInicio = document.getElementById('fechaInicio').value
     const fechaFin = document.getElementById('fechaFin').value
-    const adjunto = document.getElementById('customFile').value
-    if (!motivo.length || !fechaInicio.length || !fechaFin.length || !adjunto.length) {
+    const adjunto = document.getElementById('customFile').value   
+    if (!motivo.length || !fechaInicio.length || !fechaFin.length ) {
         alert("Complete todos los campos")
     } else {
+        // uploadFile()
         db.collection("incapacidad").add({
             motivo_incapacidad: motivo,
             fecha_inicio: fechaInicio,
@@ -38,6 +50,7 @@ function saveData() {
             let errorMessage = error.message;
             alert(errorMessage)
         });
+
     }
 
 }
@@ -76,6 +89,14 @@ function loadData() {
         alert("Error getting document:", error);
     });
 }
+
+// function uploadFile() {
+//     let storageRef = storage.ref('/my_files/' + myFile.name)
+//     let task = storageRef.put(file)
+//     task.on('state_changed', (snapShot) => {}, (error) => {alert(error)}, () => {
+//         console.log('exito')
+//     })
+// }
 
 
 // delete data 
